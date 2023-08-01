b097da13884c4c10a27a6cc6eeb3ff1f92d1c159
Tommaso Bonvicini <tommasobonvicini@gmail.com>
2023-08-01 14:18:17 +0200

    Update sinecrunch.hpp

--- sinecrunch.hpp
+++ sinecrunch.hpp
@@ -1,12 +1,13 @@
 #pragma once
+#include<stdint.h>
 
-#include <math.h>
-
-namespace sinecrunch 
+// 32bit unitary angle.
+struct ungle_t
 {
-	template <int grade, typename T>
-	T sin (T angle) 
-	{
-		return angle;
-	}
-}
+	constexpr ungle_t()noexcept:value(0){}
+	constexpr ungle_t(ungle_t u)noexcept:value(u.value){}
+	constexpr ungle_t(uint32_t u)noexcept:value(static_cast<uint32_t>((1UL<<32UL)*(u%360)/360)){}
+	constexpr ungle_t(int32_t i)noexcept:value(){}
+	constexpr ungle_t(float f)noexcept:value(){}
+	uint32_t value;
+};
